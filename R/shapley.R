b6a865dc32241f0c468f348e85056476a58df934
Katherine Pulham <katepulham44@gmail.com>
2022-12-01 20:04:52 -0500

    initial commit setup

---
+++ R/shapley.R
@@ -0,0 +1,5 @@
+#This function takes in a response vector, a design matrix, and calculates the
+#shapley value for each column of the design matrix using either AIC or R^2.
+shapley <- function(Y,X,score="AIC"){
+  return(NULL)
+}
